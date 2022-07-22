d11c984db5131e10c07281c9cdcb65bf8d927a7d
Carlos Abreu <iatsebrother@gmail.com>
2022-07-22 15:26:23 -0700

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xDa2Ed4aC95DD564115cBb72c7B87d88e5d091395"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
