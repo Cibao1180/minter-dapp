686ea4d6ea6eeda829ff1e7fa7351e6574fe2afe
Carlos Abreu <iatsebrother@gmail.com>
2022-07-23 23:02:13 -0700

    updated mint price

--- backend/src/config.js
+++ backend/src/config.js
@@ -63,7 +63,7 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xbcabA2ef566D8035b74e0a884C99DC4572Fd0452';
 const TREASURY_ADDRESS = '0xbcabA2ef566D8035b74e0a884C99DC4572Fd0452';
 const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 0.001; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const MINT_PRICE = 0.01; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 10; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
